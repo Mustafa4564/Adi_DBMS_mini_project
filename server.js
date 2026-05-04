ec64c430697e43ebae49993726c482fa86a1dde1
Mustafa4564 <mustafa.ansari24@mmit.edu.in>
2026-05-04 09:23:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(__dirname));
+
+// Serve index.html on root
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 // MySQL Connection Pool
 const pool = mysql.createPool({
